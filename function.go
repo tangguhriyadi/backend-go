b250a30a4cab70d616cb147c9fca6373a6d808b3
tangguhriyadi <mtangguh97@gmail.com>
2023-02-08 09:27:37 +0700

    variadic function

--- function.go
+++ function.go
@@ -1,4 +1,4 @@
-package main
+/* package main
 
 import "fmt"
 
@@ -42,3 +42,4 @@ func main() {
 	fmt.Println(a, b, c)
 
 }
+*/
---
+++ variadicFunction.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func sumAll(numbers ...int) int { // numbers dikonversi menjadi slice
+	total := 0
+
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+func main() {
+	total := sumAll(10, 20, 30, 40, 50)
+
+	numbers := []int{10, 20, 30, 40}
+
+	totals := sumAll(numbers...)
+
+	fmt.Println(total)
+	fmt.Println(totals)
+}
