b24d70d0b66f4fbbd51266a017e2535d5b7ffa30
tangguhriyadi <mtangguh97@gmail.com>
2023-02-08 08:30:57 +0700

    function return multiple value

--- function.go
+++ function.go
@@ -10,6 +10,10 @@ func hasReturn(name string) string {
 	return "my name is " + name
 }
 
+func getFullname() (string, string) { //return multiple value
+	return "tangguh", "riyadi"
+}
+
 func main() {
 	for i := 0; i <= 10; i++ {
 		sayHello(i)
@@ -18,4 +22,10 @@ func main() {
 	namaSaya := hasReturn("tangguh")
 
 	fmt.Println(namaSaya)
+
+	firstName, lastName := getFullname()
+	fmt.Println(firstName, lastName)
+
+	first, _ := getFullname()
+	fmt.Println(first)
 }
