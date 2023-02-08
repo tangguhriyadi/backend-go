b142e1f2adaf2158720c4072aae39cf78ed9abd5
tangguhriyadi <mtangguh97@gmail.com>
2023-02-08 08:39:43 +0700

    function return named value

--- function.go
+++ function.go
@@ -14,6 +14,14 @@ func getFullname() (string, string) { //return multiple value
 	return "tangguh", "riyadi"
 }
 
+func getCompleteName() (pertama, kedua, ketiga string) { // returning named value
+	pertama = "muhammad"
+	kedua = "tangguh"
+	ketiga = "riyadi"
+
+	return
+}
+
 func main() {
 	for i := 0; i <= 10; i++ {
 		sayHello(i)
@@ -28,4 +36,9 @@ func main() {
 
 	first, _ := getFullname()
 	fmt.Println(first)
+
+	a, b, c := getCompleteName()
+
+	fmt.Println(a, b, c)
+
 }
